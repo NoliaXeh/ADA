14b0ee674964e981b7cc75630b3b2d5bf28b6753
Lionel Vincke <lionel.vincke@epita.fr>
2021-02-02 15:30:00 +0100

    add api graphic

--- src/graphics.adb
+++ src/graphics.adb
@@ -1,5 +1,20 @@
+with Maps; use Maps;
+with Gtk.Fixed;       use Gtk.Fixed;
+with Gtk.Image;       use Gtk.Image;
+with Game; use Game;
 package body Graphics is
 
-   
+   procedure fill_screen(map: in Maps.Map) is
+   begin
+      Maps.fill_screen(map);
+   end fill_screen;
+   procedure set_background(image: in String) is 
+      img : Gtk_Image;
+   begin
+      Gtk_New (img, image);
+      Game.Fixed_Back.Put(Widget => img,
+                 X      => 0,
+                 Y      => 0);
+   end set_background;
 
 end Graphics;
--- src/graphics.ads
+++ src/graphics.ads
@@ -1,5 +1,8 @@
+with Maps; use Maps;
+with Gtk.Fixed;       use Gtk.Fixed;
 package Graphics is
 
-   
+   procedure fill_screen(map: in Maps.Map);
+   procedure set_background(image: in String);
 
 end Graphics;

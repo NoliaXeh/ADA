99bfdab96993d36ec4346ab826432143ab52282a
Leo Tordjman <leo.tordjman@epita.fr>
2021-02-05 17:28:40 +0100

    add friction and placeBlock()

--- src/g_obj/block.adb
+++ src/g_obj/block.adb
@@ -1,22 +1,41 @@
---with Sprite;
+with Gdk.Pixbuf; use Gdk.Pixbuf;
+with Glib.Error; use Glib.Error;
+with Glib; use Glib;
 
 package body Block is
    
-   --procedure placeBlock (Self : in Block; Layer : in out Gtk_Fixed) is
-   --begin
-      --Layer.Put(Widget => Sprite.getSprite(Self.getSpriteId),
-       --         X      => Self.getMesh.Position.X,
-     --           Y      => Self.getMesh.Position.Y);
-   --end placeBlock;
+   procedure placeBlock (Self : in Block; Layer : in out Gtk_Fixed) is
+      Buff : Gdk_Pixbuf;
+      Img : Gtk_Image;
+      err : GError;
+   begin
+      -- Load image
+      Gdk_New_From_File(Pixbuf   => Buff,
+                        Filename => Self.getSpritePath,
+                        Error    => err);
+      -- Create widget
+      Gtk_New(Img, Buff);
+      -- Put the new widget inside the Layer
+      Layer.Put(Widget => Img,
+                X      => Gint(Self.getEntity.Position.X),
+                Y      => Gint(Self.getEntity.Position.Y));
+   end placeBlock;
 
    -- getter
-   function getNature(Self : in Block) return Integer is (Self.Nature);
+   function getNature (Self : in Block) return Integer is (Self.Nature);
+   function getFriction (Self : in Block) return Float is (Self.Friction);
    
    -- setter
    procedure setNature (Self : in out Block; Value : in Integer) is
    begin
       Self.Nature := Value;
    end setNature;
+   
+   
+   procedure setFriction (Self : in out Block; Value : in Float) is
+   begin
+      Self.Friction := Value;
+   end setFriction;
    
    -- private
 
--- src/g_obj/block.ads
+++ src/g_obj/block.ads
@@ -10,18 +10,22 @@ package Block is
    type Block is new GameObject.GameObject with private;
    
    -- Put the block inside the Layer
-   --procedure placeBlock (Self : in Block; Layer : in out Gtk_Fixed);
+   procedure placeBlock (Self : in Block; Layer : in out Gtk_Fixed);
    
    -- getter
    function getNature(Self : in Block) return Integer;
+   function getFriction (Self : in Block) return Float;
    
    -- setter
    procedure setNature(Self : in out Block; Value : in Integer);
+   procedure setFriction (Self : in out Block; Value : in Float)
+     with Pre => Value >= 0.0;
    
 private
    type Block is new GameObject.GameObject with record
       
       Nature : Integer := 0; -- Sand, Ground, Water, ...
+      Friction : Float := 0.5;
       
    end record;
 

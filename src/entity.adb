3e8351ef0c541624c8ea2e1a66d17f0a387173f7
Lionel Vincke <lionel.vincke@epita.fr>
2021-02-07 17:43:32 +0100

    Final fix physics

--- src/entity.adb
+++ src/entity.adb
@@ -75,7 +75,7 @@ is
          Player.Forces.X := -Player.Forces.X;
       else
          -- Bottom Collision
-         B := Maps.Get(X => X,
+         B := Maps.Get(X => Integer(Player.Position.X - 32.0) / 64,
                     Y => Y + 1);
          if B.getNature /= 2 and then Collides(B.getEntity, Player) then
             Apply_Force(Player, (0.0, -Player.Forces.Y));
